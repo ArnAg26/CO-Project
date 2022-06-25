03ba6f1aa76777e12e57017fa426d3d04e77543d
ArnAg26 <94694351+ArnAg26@users.noreply.github.com>
2022-06-25 19:35:33 +0530

    Add files via upload
    All errors handled which are not due to incorrect syntax of an instruction like illegal use of flags,immediate values, label errors etc
    A ErrorCheck fn created for checking all these errors together and return No Error if the code is correct

--- ErrorChecking.py
+++ ErrorChecking.py
@@ -13,7 +13,7 @@ printInput(lst)
 
 def haltError(lst):
     if lst[-1]!=['hlt']:
-        return -1
+        return 0
     for i in range(len(lst)-1):
             if lst[i]==['hlt']:
                 return -1
@@ -21,7 +21,7 @@ def haltError(lst):
 
 #print(haltError(lst))
 varlist=[]
-def VarList(lst):
+def VarList(lst,varlist):
     i=0
     while lst[i][0]=='var':
         if len(lst[i])>2:
@@ -34,22 +34,107 @@ def VarList(lst):
             return -1
     return 1
 
-def UndefinedVariables(lst,varlist):
+labellist=[]
+
+def Labellist(lst,labellist):
+    i=0
+    for i in range(len(lst)):
+        if len(lst[i])>1 and lst[i][1][-1]==":":
+            return -1
+        elif lst[i][0][-1]==":":
+            labellist.append(lst[i][0][:-1])
+            return 1
+            
+
+def UndefinedVariables(lst,varlist,labellist):
     for i in range(len(lst)):
         if lst[i][0] in ["ld","st"]:
             y=lst[i][-1]
-            if y not in varlist:
+            if y not in varlist and y not in labellist:
                 return -1
-            else:
-                return 1
+            elif y not in varlist and y in labellist:
+                return 0
+    return 1
+            
+            
+            
+def UndefinedLabels(lst,varlist,labellist):
+    for i in range(len(lst)):
+        if lst[i][0] in ["jmp","jgt","je","jlt"]:
+            y=lst[i][-1]
+            if y not in labellist and y not in varlist:
+                return -1
+            elif y not in labellist and y in varlist:
+                return 0
+    return 1
+                
         
+def ImmediateError(lst):
+    for i in range(len(lst)):
+        if lst[i][0]=="mov" and lst[i][2][0]=="$":
+            if int(lst[i][2][1:])<0 or int(lst[i][2][1:])>255:
+                return -1
+        elif lst[i][0] in ["ls","rs"]:
+            if lst[i][2][0]!="$":
+                return 0
+            elif int(lst[i][2][1:])<0 or int(lst[i][2][1:])>255:
+                return -1
+    return 1
 
-if VarList(lst)==1:
-    print(varlist)
-else:
-    print(VarList(lst))
-    
-print(UndefinedVariables(lst,varlist))
+def IllegalFlag(lst):
+    for i in range(len(lst)):
+        if lst[i][0] in ["add","sub","mul","xor","or","and"]:
+            if lst[i][1]=="FLAGS" or lst[i][2]=="FLAGS" or lst[i][3]=="FLAGS:":
+                return -1
+        elif lst[i][0] in ["cmp","not","div"]:
+            if lst[i][1]=="FLAGS" or lst[i][2]=="FLAGS":
+                return -1
+        elif lst[i][0] in ["mov","ld","st","rs","ls"]:
+            if lst[i][1]=="FLAGS":
+                return -1
+    return 1
+
+def ErrorCheck(lst):
+    varlist=[]
+    labellist=[]
+    if VarList(lst,varlist)==1:
+        print(varlist)
+    else:
+        return "Variables not defined at start"
+    if Labellist(lst,labellist)==1:
+        print(labellist)
+    else:
+        return "Space between label and colon"
+    z=ImmediateError(lst)
+    if z==0:
+        return "$ does not preceed immediate value"
+    if z==-1:
+        return "Immediate given out of range"
+    z=UndefinedVariables(lst,varlist,labellist)
+    if z==-1:
+        return "Undefined Variable"
+    if z==0:
+        return "Misuse of label as variable"
+    z=UndefinedLabels(lst,varlist,labellist)
+    if z==-1:
+        return "Undefined Label"
+    if z==0:
+        return "Misuse of variable as label"
+    z=haltError(lst)
+    if z==-1:
+        return "Halt used before last instruction"
+    if z==0:
+        return "Halt not used to terminate program"
+    if IllegalFlag(lst)==-1:
+        return "Illegal usage of flag"
+    return "No Errors"
+
+print(ErrorCheck(lst))
+
+            
+            
+
+
 
             
         
