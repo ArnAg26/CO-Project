5884ca99b7e820a6844c7c200ef386c94a30f7e7
parthivdholaria <97287981+parthivdholaria@users.noreply.github.com>
2022-07-26 11:04:59 +0530

    Q4 scatter plot

---
+++ Q4.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+import simplesimulator as simp
+
+# print(simp.x_axis)
+# print(simp.y_axis)
+
+
+plt.scatter(simp.x_axis,simp.y_axis)
+plt.title("Memory address v/s clock cycle")
+plt.xticks(simp.x_axis)
+plt.yticks(simp.y_axis)
+plt.xlabel("clock cycle")
+plt.ylabel("Memory address")
+plt.show()
